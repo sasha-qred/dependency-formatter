ebe0e6b311fed0c68a307d74fa8ef2dc2aeaa020
sasha-qred <sashaqred@ya.ru>
2017-08-03 22:54:17 +0300

    Add autosave option to settings. Closes #1.

--- src/extension.js
+++ src/extension.js
@@ -1,18 +1,27 @@
 const vscode = require('vscode'); // eslint-disable-line import/no-unresolved
 const df = require('./default-format');
 
+const config = vscode.workspace.getConfiguration('dependency-formatter');
+
 function activate(context) {
   const disposable = vscode.commands.registerCommand('dependency-formatter.formatDependencies', () => {
     try {
-      const txt = vscode.window.activeTextEditor.document.getText();
-      const totalLines = vscode.window.activeTextEditor.document.lineCount;
-      vscode.window.activeTextEditor
-        .edit((editBuilder) => {
-          const docStart = new vscode.Position(0, 0);
-          const docEnd = new vscode.Position(totalLines + 1, 0);
-          const fullDocRange = new vscode.Range(docStart, docEnd);
-          editBuilder.replace(fullDocRange, df.defaultFormat(txt));
-        });
+      const activeEditor = vscode.window.activeTextEditor;
+      activeEditor.edit((editBuilder) => {
+        const docText = activeEditor.document.getText();
+        const totalLines = activeEditor.document.lineCount;
+        const docStart = new vscode.Position(0, 0);
+        const docEnd = new vscode.Position(totalLines + 1, 0);
+        const fullDocRange = new vscode.Range(docStart, docEnd);
+        const updatedDocText = df.defaultFormat(docText);
+        if (updatedDocText !== docText) {
+          editBuilder.replace(fullDocRange, updatedDocText);
+        }
+      }).then(() => {
+        if (config.get('autosave')) {
+          activeEditor.document.save();
+        }
+      });
     } catch (e) {
       vscode.window.showErrorMessage('Error');
     }
